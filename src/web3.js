fcc181f8c1c97a95933ff5362bef19320e3dad36
Hammad Mustafa <tohammad@gmail.com>
2022-10-12 15:17:15 +0530

    metaverse

---
+++ src/web3.js
@@ -0,0 +1,49 @@
+import abi from "./abi/abi.json" assert { type: "json" };
+
+const polygon = new Promise((res, rej) => {
+  async function meta() {
+    if (typeof window.ethereum == "undefined") {
+      rej("You should install Metamask");
+    }
+
+    let web3 = new Web3(window.ethereum);
+    let contract = new web3.eth.Contract(
+      abi,
+      "0x05d366DfAd13134C9a02de34048FE5fBFAb77D70"
+    );
+
+    let accounts = await web3.eth.requestAccounts(); //array of accounts
+    console.log("Connected account:", accounts[0]);
+
+    let totalSupply = await contract.methods
+      .totalSupply()
+      .call({ from: accounts[0] });
+    console.log("Total Supply", totalSupply);
+
+    let maxSupply = await contract.methods
+      .maxSupply()
+      .call({ from: accounts[0] });
+    console.log("Max Supply", maxSupply);
+
+    let objects = await contract.methods
+      .getOwnerObjects()
+      .call({ from: accounts[0] });
+    console.log("Your objects", objects);
+
+    web3.eth.requestAccounts().then((accounts) => {
+      contract.methods
+        .totalSupply()
+        .call({ from: accounts[0] })
+        .then((supply) => {
+          contract.methods
+            .getObjects()
+            .call({ from: accounts[0] })
+            .then((data) => {
+              res({ supply: supply, nft: data });
+            });
+        });
+    });
+  }
+  meta();
+});
+export default polygon;
